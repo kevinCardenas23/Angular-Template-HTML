449f1d496ef1c0ffa8e7e45905b2c6b1da2a9b04
Kevin Cardenas <kevincardenas@usantotomas.edu.co>
2020-12-22 20:53:31 -0500

    Se asociaron rutas a algunos links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { ItemComponent } from './pages/item/item.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 const routes: Routes = [
-  {path:'', component: PortafolioComponent},
+  {path:'home', component: PortafolioComponent},
   {path:'about', component: AboutComponent},
   {path:'item', component: ItemComponent},
-  {path:'**', pathMatch:'full', redirectTo:''}
+  {path:'**', pathMatch:'full', redirectTo:'home'}
 ];
 
 @NgModule({
