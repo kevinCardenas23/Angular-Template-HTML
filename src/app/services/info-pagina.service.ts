3a86c65b30c801113158fb170dfe34de3d1da49e
Kevin Cardenas <kevincardenas@usantotomas.edu.co>
2020-12-26 13:43:26 -0500

    Se conecto y recibio un objeto JSON desde Firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,16 +8,28 @@ export class InfoPaginaService {
 
   info: any = {};
   cargada = false;
+  equipo: any = []
 
   constructor(private http: HttpClient) {
-    console.log("Funciono el infopaginaService !!");
+    this.cargarInfo()
+    this.cargarEquipo()
+  }
+
+  private cargarInfo() {
     //Leer el archivo JSON para leer las propiedades
     this.http.get('assets/data/data-pagina.json').subscribe(resp => {
       this.cargada = true;
       this.info = resp
       console.log(resp)
       console.log(this.info['titulo']);
-      
+    })
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-58a46-default-rtdb.firebaseio.com/equipo.json').subscribe(resp => {
+      this.equipo = resp
+      console.log(resp) 
     })
   }
 }
+
