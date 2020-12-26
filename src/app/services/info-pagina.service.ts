843b16563d3f6a5e387cda12b76e193b04c775b0
Kevin Cardenas <kevincardenas@usantotomas.edu.co>
2020-12-26 14:54:03 -0500

    Se mostro la informacion de Firebase en Angular sin for

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -20,15 +20,13 @@ export class InfoPaginaService {
     this.http.get('assets/data/data-pagina.json').subscribe(resp => {
       this.cargada = true;
       this.info = resp
-      console.log(resp)
-      console.log(this.info['titulo']);
     })
   }
 
   private cargarEquipo() {
     this.http.get('https://angular-html-58a46-default-rtdb.firebaseio.com/equipo.json').subscribe(resp => {
       this.equipo = resp
-      console.log(resp) 
+      console.log(resp)
     })
   }
 }
