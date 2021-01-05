0c14bd92ea8dfd0a497822b9e33e8a550953a4f1
Kevin Cardenas <kevincardenas@usantotomas.edu.co>
2021-01-05 11:17:19 -0500

    Ya funciona el servicio de busqueda

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -9,12 +9,12 @@ import { ProductosService } from 'src/app/services/productos.service';
 })
 export class SearchComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, public productoService:ProductosService) {}
+  constructor(private route: ActivatedRoute, public productosService:ProductosService) {}
 
   ngOnInit(): void {
-    this.route.params
-      .subscribe(params => {
+    this.route.params.subscribe(params => {
         console.log(params['termino']);
+        this.productosService.buscarProducto(params['termino']);
       })
   }
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -7,30 +7,52 @@ import { HttpClient } from '@angular/common/http';
 export class ProductosService {
 
   cargando = true;
-  productos: any = []
-  productosFiltrado: any = []
+  productos: any = [];
+  productosFiltrado: any = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos()
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-58a46-default-rtdb.firebaseio.com/productos_idx.json').subscribe((resp) => {
-      this.productos = resp
-      this.cargando = false;
-    })
+    return new Promise<void>((resolve, reject) => {
+      this.http.get('https://angular-html-58a46-default-rtdb.firebaseio.com/productos_idx.json')
+        .subscribe((resp) => {
+          this.productos = resp;
+          this.cargando = false;
+          resolve();
+        });
+    });
   }
 
   getProducto(id: String) {
     return this.http.get(`https://angular-html-58a46-default-rtdb.firebaseio.com/productos/${id}.json`)
   }
 
-  buscarProducto(termino: String) {
+  buscarProducto(termino: string) {
+    // if (this.productos.length === 0) {
+    //   //Cargar los productos
+    //   this.cargarProductos().then(() => {
+    //     //Despues de tener los productos
+    //     //Aplciar el filtro
+    //     this.filtrarProductos(termino)
+    //   })
+
+    // } else {
+    //   //Esperar a que carguen
+    //   this.filtrarProductos(termino)
+    // }
+
     this.productosFiltrado = this.productos.filter((producto: any) => {
-      return producto;
+      return true;
     });
 
     console.log(this.productosFiltrado);
+    
 
+  }
+  private filtrarProductos(termino:string){
+      console.log(this.productosFiltrado);
+      
   }
 }
